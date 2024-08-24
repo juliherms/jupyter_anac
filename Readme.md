6d527933cb00b0db55d56cf2b81291786f7e9f36
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2024-08-24 11:10:23 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+-  python -m venv venv (criação do ambiente virtual)
+-  venv\Scripts\activate (para executar o ambiente virtual)
+-  Set-ExecutionPolicy AllSigned (libera a politica de execução do powershell)
+
+-  pip install psycopg2 # Para conexão como banco de dados postgreSQL
+-  pip install openpyxl # Para trabalhar com arquivos Excel (xlsx)
+-  pip install pandas # Para manipulação e análise de dados (df)
+
---
+++ sql/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS Anac (
+    numero_da_ocorrencia int,
+    classificacao_da_ocorrencia varchar(50),
+    data_da_ocorrencia date,
+    municipio varchar(50),
+    uf varchar(30),
+    regiao varchar(30),
+    nome_do_fabricante(100)
+)
+
+insert into anac (
+    numero_da_ocorrencia,
+    classificacao_da_ocorrencia,
+    data_da_ocorrencia,
+    municipio,
+    uf,
+    regiao,
+    nome_do_fabricante
+) VALUES (
+    1,
+    'Acidente',
+    '2022-01-01',
+    'São Paulo',
+    'SP',
+    'Sudeste',
+    'Jatinho 444')
